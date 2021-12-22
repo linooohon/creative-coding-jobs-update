fa76ff30ef5882d4855e68b40f1c0a9b9a2d1f06
linooohon <linooohon@gmail.com>
2021-12-22 16:21:20 +0800

    fix: testing for log

--- src/store/index.js
+++ src/store/index.js
@@ -4,17 +4,25 @@ import axios from 'axios'
 // not use right now
 export default createStore({
   state: {
-    dataArray: []
+    dataArray: [],
+    updateTime: ''
   },
   mutations: {
     assignToData (state, payload) {
       state.dataArray = payload
+    },
+    assignUpdateTime (state, payload) {
+      state.updateTime = payload
     }
   },
   actions: {
     async getTotalJobData (context) {
       const data = await axios.get('https://raw.githubusercontent.com/linooohon/creative-coding-jobs-update/main/src/final.json')
       context.commit('assignToData', data.data)
+    },
+    async getUpdateTime (context) {
+      const data = await axios.get('https://raw.githubusercontent.com/linooohon/creative-coding-jobs-update/data/update_time.log')
+      context.commit('assignUpdateTime', data.data)
     }
   },
   modules: {
