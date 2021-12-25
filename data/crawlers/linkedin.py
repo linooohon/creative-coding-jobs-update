d16b7b9ba308e59d54463a9b4834ec6982478167
linooohon <linooohon@gmail.com>
2021-12-25 13:15:15 +0800

    fix ip

--- data/crawlers/linkedin.py
+++ data/crawlers/linkedin.py
@@ -124,7 +124,7 @@ class LinkedIn(BaseCrawler):
 
         # resp = requests.get(
         #     url=url, headers=self.headers)
-        resp = requests.get(url=url, headers={'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44'})
+        resp = requests.get(url=url, headers={'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44'}, proxies={'http': '180.177.111.133', 'https': '180.177.111.133'})
         soup = BeautifulSoup(resp.text, 'html.parser')
         # print(soup)
 
