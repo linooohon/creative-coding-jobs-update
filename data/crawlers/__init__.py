d1ea6247988205ab89a01ebdbf88cf39f0b7b531
linooohon <linooohon@gmail.com>
2021-12-26 15:37:36 +0800

    fix

--- data/crawlers/__init__.py
+++ data/crawlers/__init__.py
@@ -3,4 +3,4 @@ from .linkedin_selenium import LinkedIn
 from .simplyhired import SimplyHired
 from .glassdoor import Glassdoor
 
-JOB_BANK_LIST = [LinkedIn, Indeed, SimplyHired, Glassdoor]
+JOB_BANK_LIST = [Indeed, SimplyHired, Glassdoor]
