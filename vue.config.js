f5481e1823aa396d9639be16425db2e966584fc2
linooohon <linooohon@gmail.com>
2021-12-29 17:14:19 +0800

    fix: add vue netlify deploy referneces

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+// References: deploy on github pages and netlify
+// https://cli.vuejs.org/guide/deployment.html#netlify
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/creative-coding-jobs-update/'
