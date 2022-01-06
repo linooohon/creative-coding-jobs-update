d6bf0c547db015820c26b0ca3b3e7d842ac4a8fa
linooohon <linooohon@gmail.com>
2022-01-06 15:54:37 +0800

    fix: testing config file format

--- vue.config.js
+++ vue.config.js
@@ -2,20 +2,8 @@
 // https://cli.vuejs.org/guide/deployment.html#netlify
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/creative-coding-jobs-update/'
-    : '/'
-}
-
-module.exports = {
-  pwa: {
-    workboxOptions: {
-      exclude: [/_redirects/]
-    }
-  }
-}
-
-module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/creative-coding-jobs-update/' : '/',
+  pwa: { workboxOptions: { exclude: [/_redirects/] } },
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = 'Creative Coding Jobs Daily Update'
@@ -23,3 +11,20 @@ module.exports = {
     })
   }
 }
+
+// module.exports = {
+//   pwa: {
+//     workboxOptions: {
+//       exclude: [/_redirects/]
+//     }
+//   }
+// }
+
+// module.exports = {
+//   chainWebpack: config => {
+//     config.plugin('html').tap(args => {
+//       args[0].title = 'Creative Coding Jobs Daily Update'
+//       return args
+//     })
+//   }
+// }
