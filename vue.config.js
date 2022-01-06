3dc79c04281623c441d7420d2c43f91d36489444
linooohon <linooohon@gmail.com>
2022-01-06 16:52:18 +0800

    fix: fix config for deploy to netlify

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,15 @@
 // References: deploy on github pages and netlify
 // https://cli.vuejs.org/guide/deployment.html#netlify
 
+/* If use github pages use these options:
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/creative-coding-jobs-update/' : '/',
-  pwa: { workboxOptions: { exclude: [/_redirects/] } },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/creative-coding-jobs-update/'
+    : '/'
+}
+
+module.exports = {
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = 'Creative Coding Jobs Daily Update'
@@ -12,19 +18,20 @@ module.exports = {
   }
 }
 
-// module.exports = {
-//   pwa: {
-//     workboxOptions: {
-//       exclude: [/_redirects/]
-//     }
-//   }
-// }
+*/
 
-// module.exports = {
-//   chainWebpack: config => {
-//     config.plugin('html').tap(args => {
-//       args[0].title = 'Creative Coding Jobs Daily Update'
-//       return args
-//     })
-//   }
-// }
+// Netlify
+module.exports = {
+  publicPath: '/',
+  pwa: {
+    workboxOptions: {
+      exclude: [/_redirects/]
+    }
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Creative Coding Jobs Daily Update'
+      return args
+    })
+  }
+}
